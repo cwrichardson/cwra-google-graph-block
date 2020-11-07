736d9e64a4112906e2f081b3b5b3f208871a6153
Christopher Richardson <cwr@mirovoysales.com>
2020-11-07 10:00:34 +0100

    Cleanup formatting.

--- public/class-cwra-google-graph-block-public.php
+++ public/class-cwra-google-graph-block-public.php
@@ -72,7 +72,7 @@ class CWRA_Google_Graph_Block_Public {
 	private function date_version( $suffix ) {
 		return $this->version . '.'
 		    . filemtime( plugin_dir_path( __FILE__ )
-		        . $suffix );
+		    . $suffix );
 	}
 
 	/**
@@ -82,19 +82,10 @@ class CWRA_Google_Graph_Block_Public {
 	 */
 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in CWRA_Google_Graph_Block_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The CWRA_Google_Graph_Block_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/cwra-google-graph-block-public.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name,
+		    plugin_dir_url( __FILE__ )
+		        . 'css/cwra-google-graph-block-public.css',
+		    array(), $this->version, 'all' );
 
 	}
 
