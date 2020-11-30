1bb91fa95b6ceae3b1a32ddcefde12c459bf8258
Christopher Richardson <cwr@mirovoysales.com>
2020-11-30 15:01:46 +0100

    Add constants.js. Change translation domain to 'cwraggb'. Start working on configuration parameters for charts. Very early; moving into classes now.

---
+++ admin/src/constants.js
@@ -0,0 +1,22 @@
+export const CHART_TYPES = {
+	'area': 'Area Chart',
+	'bar': 'Bar Chart',
+	'bubble': 'Bubble Chart',
+	'candlestick': 'Candlestick Chart',
+	'column': 'Column Chart',
+	'donut': 'Donut Chart',
+	'combo': 'Combo Chart',
+	'gauge': 'Gauge',
+	'geo': 'Geo Chart',
+	'histogram': 'Histogram',
+	'line': 'Line Chart',
+	'org': 'Org Chart',
+	'pie': 'Pie Chart',
+	'scatter': 'Scatter Chart',
+	'steppedarea': 'Stepped Area Chart',
+	'table': 'Table',
+	'timeline': 'Timeline',
+	'treemap': 'Tree Map',
+};
+export const DEFAULT_HEIGHT = 720;
+export const DEFAULT_WIDTH = 1200;
--- admin/src/edit.js
+++ admin/src/edit.js
@@ -4,19 +4,76 @@
 
 import { __, sprintf } from '@wordpress/i18n';
 import apiFetch from '@wordpress/api-fetch';
-import {
-	InspectorControls,
-	URLInput,
-	useBlockProps } from '@wordpress/block-editor';
+import { InspectorControls } from '@wordpress/block-editor';
 import {
 	Button,
 	PanelBody,
 	PanelRow,
 	TextControl,
+	ToggleControl,
 	SelectControl,
 	Spinner} from '@wordpress/components';
 import { useDispatch } from '@wordpress/data';
-import { Fragment, useState } from '@wordpress/element';
+import { Fragment, useCallback, useState } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { CHART_TYPES } from './constants';
+
+function ChartEdit( { title = '', userCanEdit = false, chartType,
+    setAttributes } ) {
+
+	function setTextAttr ( attr, value ) {
+		setAttributes( { [ attr ]: value } );
+	}
+
+	function onSetTitle ( value ) {
+		setTextAttr('cwraggTitle', value);
+	}
+
+	function onSetHAxisTitle ( value ) {
+		return;
+		/*( setTextAttr('cwraggHAxisTitle', value);*/
+	}
+
+	function onSetVAxisTitle ( value ) {
+		return;
+		/*setTextAttr('cwraggHAxisTitle', value);*/
+	}
+
+	return (
+	    <>
+		<div>
+			<TextControl
+			    label={ __( 'Title', 'cwraggb' ) }
+			    value={ title || '' }
+			    onChange={ onSetTitle }
+			/>
+			<TextControl
+			    label={ __( 'Horizontal Axis Title', 'cwraggb' ) }
+			    value={ /*cwraggHAxisTitle || ''*/ '' }
+			    onChange={ onSetHAxisTitle }
+			/>
+			<TextControl
+			    label={ __( 'Vertical Axis Title', 'cwraggb' ) }
+			    value={ /*cwraggVvAxisTitle || ''*/ '' }
+			    onChange={ onSetVAxisTitle }
+			/>
+			<ToggleControl
+			    label={ __( 'Allow user interaction', 'cwraggb' ) }
+			    help={ userCanEdit
+			        ? __( 'Google visualization controls enabled.',
+				    'cwraggb' )
+				: __( 'Google visualization controls disabled.',
+				    'cwraggb' ) }
+			    checked={ userCanEdit }
+			    onChange={ () => setAttributes( { cwraggUserCanEdit:
+			        ! userCanEdit } ) } />
+		</div>
+	    </>
+	);
+}
 
 export default function CwraGoolgeGraphEdit( props ) {
 	const {
@@ -24,7 +81,9 @@ export default function CwraGoolgeGraphEdit( props ) {
 		    cwraggChartType,
 		    cwraggDataSourceType,
 		    cwraggDataSource,
-		    cwraggLocalFile
+		    cwraggLocalFile,
+		    cwraggTitle,
+		    cwraggUserCanEdit
 		},
 		setAttributes,
 	} = props;
@@ -33,6 +92,16 @@ export default function CwraGoolgeGraphEdit( props ) {
 	const [ isValidating, setIsValidating ] = useState( false );
 	const post_id = wp.data.select("core/editor").getCurrentPostId();
 	console.log('Post ID is ', post_id);
+
+	// fill out the key-value pairs for the select chart-type dropdown
+	let chartSelectType = [];
+	if (CHART_TYPES) {
+		for (const key in CHART_TYPES) {
+			chartSelectType.push( { value: __( key, 'cwraggb' ),
+			    label: CHART_TYPES[key] }
+			);
+		}
+	}
 
 	function validate() {
 		setIsValidating( true );
@@ -52,8 +121,8 @@ export default function CwraGoolgeGraphEdit( props ) {
 			console.log('doh!', error);
 			createErrorNotice(
 				sprintf(
-				    __( 'Could not validate data source. %s' ),
-				    error.message),
+				    __( 'Could not validate data source. %s',
+				        'cwraggb' ), error.message),
 				{
 					id: 'cwragg-validate-error',
 					type: 'snackbar'
@@ -69,7 +138,7 @@ export default function CwraGoolgeGraphEdit( props ) {
 	    <Fragment>
 	    	<InspectorControls>
 		    <PanelBody
-		      title={__( 'Data Configuration' )}
+		      title={ __( 'Data Configuration', 'cwraggb' )}
 		      initialOpen={ true }>
 		    	<PanelRow>
 			    <TextControl
@@ -99,20 +168,17 @@ export default function CwraGoolgeGraphEdit( props ) {
 		    <SelectControl
 		      label={ __( 'Chart Type', 'cwraggb') }
 		      value={ cwraggChartType }
-		      options={[
-		          { label: __( 'Area', 'cwraggb' ), value: 'area' },
-		          { label: __( 'Bar', 'cwraggb' ), value: 'bar' },
-		          { label: __( 'Column', 'cwraggb' ), value: 'column' },
-		          { label: __( 'Line', 'cwraggb' ), value: 'line' },
-		          { label: __( 'Pie', 'cwraggb' ), value: 'pie' },
-		          { label: __( 'Scatter', 'cwraggb' ),
-			      value: 'scatter' }
-		      ]}
+		      options={ chartSelectType }
 		      onChange={ (chartType) => {
 		      	setAttributes( {
 			  cwraggChartType: chartType } )
 		      }} />
 		</div>
+		<ChartEdit
+		    title={ cwraggTitle }
+		    userCanEdit={ cwraggUserCanEdit }
+		    chartType={ cwraggChartType }
+		    setAttributes={ setAttributes } />
 		<div className={ "it_worked" }>Generating graph from 
 		  { cwraggDataSource }</div>
 		<div>Local data source is { cwraggLocalFile }</div>
